1356d308a20a99a0af29f7163a1765b93131359b
Akshay Kumar <akshay.gkumar37949@gmail.com>
2020-02-22 16:41:47 -0800

    Initial Commit

---
+++ Foodology/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foodology
+//
+//  Created by Akshay Kumar on 2/22/20.
+//  Copyright © 2020 WHSDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
